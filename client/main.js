9ce1f298a941e4ec2de7dbca794be4d313da3030
60029601 <a60029601@60029601ui-Mac-mini.local>
2018-12-31 17:10:13 +0900

    v1.0

--- client/main.js
+++ client/main.js
@@ -1,22 +1,217 @@
+/**
+* Created by Soto
+* Copyright @ SotoLAB Since 2015. All rights reserved.
+*/
+
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+var DEBUG = 1;
+
+
+FlowRouter.route('/', {
+  action: function () {
+    BlazeLayout.render('home', { content: 'mainInfo' });
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+FlowRouter.route('/block/:blockId', {
+  action: function (params, queryParams) {
+    if (DEBUG) console.log("block:", params.blockId);
+    Session.set('blockId', params.blockId);
+    BlazeLayout.render('home', { content: 'blockInfos' });
+  }
+});
+
+FlowRouter.route('/address/:addressId', {
+  action: function (params, queryParams) {
+    if (DEBUG) console.log("address:", params.addressId);
+    Session.set('addressId', params.addressId);
+    BlazeLayout.render('home', { content: 'addressInfo' });
+  }
+});
+
+FlowRouter.route('/transaction/:transactionId', {
+  action: function (params, queryParams) {
+    if (DEBUG) console.log("transaction:", params.transactionId);
+    Session.set('transactionId', params.transactionId);
+    BlazeLayout.render('home', { content: 'transactionInfos' });
+  }
+});
+
+/*
+     Template.mainInfo
+*/
+Template.mainInfo.helpers({
+  blockNum() {
+    if (DEBUG) console.log("blockNum++");
+    Meteor.call('blockNumber', function (error, result) {
+      if (error) {
+        if (DEBUG) console.log(error);
+        alert('error !');
+        return;
+      } else {
+        Session.set('number', result);
+      }
+    })
+    return Session.get('number');
+  },
+  blockLists() {
+    Meteor.call('blockLists', function (error, result) {
+      if (error) {
+        if (DEBUG) console.log(error);
+        alert('error !');
+        return;
+      } else {
+        Session.set('blockLists', result);
+      }
+    })
+    return Session.get('blockLists');
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+/*
+     Template.blockInfos
+*/
+Template.blockInfos.helpers({
+  getBlockHash() {
+    var blockId = Session.get('blockId');
+    if (DEBUG) console.log("hashValue:" + blockId);
+    Meteor.call('getBlockHash', blockId, function (error, result) {
+      if (error) {
+        if (DEBUG) console.log(error);
+        alert('error !');
+        return;
+      } else {
+        Session.set('hashValue', result);
+        // if (DEBUG) console.log("result.hash:" + result.hash);
+        if (DEBUG) console.log("result.conf:" + result.conf);
+      }
+    })
+    return Session.get('hashValue');
+  },
+  transactions() {
+    var blockId = Session.get('blockId');
+    if (DEBUG) console.log("transactions:" + blockId);
+    Meteor.call('transactions', blockId, function (error, result) {
+      if (error) {
+        if (DEBUG) console.log(error);
+        alert('error !');
+        return;
+      } else {
+        Session.set('transactions', result);
+        // if (DEBUG) console.log("result.hash:" + result.hash);
+        if (DEBUG) console.log("transactions.id:" + result.id);
+      }
+    })
+    return Session.get('transactions');
+  },
+
+});
+
+/*
+     Template.addressInfo
+*/
+Template.addressInfo.helpers({
+  addressId() {
+    return Session.get('addressId');
+  },
+  balance() {
+    Meteor.call('balance', Session.get('addressId'), function (error, result) {
+      if (error) {
+        if (DEBUG) console.log(error);
+        alert('error !');
+        return;
+      } else {
+        Session.set('balance', result);
+        // if (DEBUG) console.log("result.hash:" + result.hash);
+        if (DEBUG) console.log("balance:" + result);
+      }
+    })
+    return Session.get('balance');
+  },
+  balanceInEther() {
+    Meteor.call('balanceInEther', Session.get('addressId'), function (error, result) {
+      if (error) {
+        if (DEBUG) console.log(error);
+        alert('error !');
+        return;
+      } else {
+        Session.set('balanceInEther', result);
+        // if (DEBUG) console.log("result.hash:" + result.hash);
+        if (DEBUG) console.log("balanceInEther:" + result);
+      }
+    })
+    return Session.get('balanceInEther');
   },
 });
+
+/*
+     Template.transactionInfos
+*/
+Template.transactionInfos.helpers({
+  txId() {
+    return Session.get('transactionId');
+    // instance.counter.set(instance.counter.get() + 1);
+    // return Template.instance().counter.get();
+  },
+  getTransaction() {
+    // Session.set('getTransaction', false);
+    Meteor.call('getTransaction', Session.get('transactionId'), function (error, result) {
+      if (error) {
+        if (DEBUG) console.log(error);
+        alert('error !');
+        return;
+      } else {
+        Session.set('getTransaction', result);
+        // if (DEBUG) console.log("result.hash:" + result.hash);
+        if (DEBUG) console.log("getTransaction.txprice:" + result.txprice);
+      }
+    })
+    return Session.get('getTransaction');
+  },
+
+});
+
+/*
+     Template.search
+*/
+Template.search.events({
+  'submit form'(event) {
+    event.preventDefault();
+    var ethRequest = event.target.requestType.value;
+    var requestStr = ethRequest.split('0x').join('');
+
+    if (DEBUG) console.log("requestStr.length:" + requestStr.length);
+
+    if (requestStr.length == 40)
+      return goToAddrInfos(requestStr)
+    else if (requestStr.length == 64) {
+      if (/[0-9a-zA-Z]{64}?/.test(requestStr))
+        return goToTxInfos('0x' + requestStr)
+      else if (/[0-9]{1,7}?/.test(requestStr))
+        return goToBlockInfos(requestStr)
+    } else if (parseInt(requestStr) > 0)
+      return goToBlockInfos(parseInt(requestStr))
+
+    alert('Don\'t know how to handle ' + requestStr)
+
+  },
+});
+
+function goToBlockInfos(requestStr) {
+  if (DEBUG) console.log("goToBlockInfos:" + requestStr);
+  FlowRouter.go('/block/' + requestStr);
+}
+
+function goToAddrInfos(requestStr) {
+  if (DEBUG) console.log("goToAddrInfos:" + requestStr);
+  FlowRouter.go('/address/' + requestStr);
+}
+
+function goToTxInfos(requestStr) {
+  if (DEBUG) console.log("goToTxInfos:" + requestStr);
+  FlowRouter.go('/transaction/' + requestStr);
+}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,149 @@
+/**
+* Created by Soto
+* Copyright @ SotoLAB Since 2015. All rights reserved.
+*/ 
+
 import { Meteor } from 'meteor/meteor';
+import Web3 from 'web3';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+var DEBUG = 1;
+
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+  if (DEBUG) console.log('web3');
+} else {
+  // set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  if (DEBUG) console.log('HttpProvider!');
+}
+
+Meteor.methods({
+  'blockNumber': function () {
+    var blockNumber = web3.eth.blockNumber;
+    if (DEBUG) console.log("result :" + blockNumber);
+    return blockNumber;
+  },
+
+  'blockLists': function () {
+    var maxBlocks = 15; // TODO: into setting file or user select
+    var blockNum = web3.eth.blockNumber;
+    if (maxBlocks > blockNum) {
+      maxBlocks = blockNum + 1;
+    }
+
+    // get latest 50 blocks
+    var blocks = [];
+    for (var i = 0; i < maxBlocks; ++i) {
+      blocks.push(web3.eth.getBlock(blockNum - i));
+    }
+    // if (DEBUG) console.log("blocks :" + blocks);
+    return blocks;
+  },
+
+  'getBlockHash': function (blockId) {
+    var number = web3.eth.blockNumber;
+    var blockInfo = web3.eth.getBlock(blockId);
+    blockInfo.difficulty = ("" + blockInfo.difficulty).replace(/['"]+/g, '');
+    blockInfo.dataFromHex = hex2a(blockInfo.extraData);
+    blockInfo.blockNumber = blockInfo.number;
+    // if (DEBUG) console.log("blockInfo :" + blockInfo);
+
+    if (blockInfo.blockNumber != undefined) {
+      blockInfo.conf = number - blockInfo.blockNumber + " Confirmations";
+      // if (DEBUG) console.log("blockInfo.conf :" + blockInfo.conf);
+      if (blockInfo.conf == 0 + " Confirmations") {
+        blockInfo.conf = 'Unconfirmed';
+
+      }
+    }
+
+    if (blockInfo.blockNumber != undefined) {
+      var info = web3.eth.getBlock(blockInfo.blockNumber);
+      if (info != undefined) {
+        var newDate = new Date();
+        newDate.setTime(info.timestamp * 1000);
+        blockInfo.time = newDate.toUTCString();
+        // if (DEBUG) console.log("blockInfo.time :" + blockInfo.time);
+      }
+    }
+    return blockInfo;
+  },
+
+  'transactions': function (blockId) {
+    var transactions = [];
+    var txCount = web3.eth.getBlockTransactionCount(blockId);
+    if (DEBUG) console.log("transactions txCount :" + txCount);
+
+    for (var blockIdx = 0; blockIdx < txCount; blockIdx++) {
+      var result = web3.eth.getTransactionFromBlock(blockId, blockIdx); 
+      if (DEBUG) console.log("transactions :" + result);
+
+      var transaction = {
+        id: result.hash,
+        hash: result.hash,
+        from: result.from,
+        to: result.to,
+        gas: result.gas,
+        input: result.input,
+        value: result.value
+      }
+      transactions.push(transaction);
+    }
+
+    return transactions;
+    // if (DEBUG) console.log("result :" + blockInfo.hash);
+    // return blockInfo.hash;
+  },
+  
+  'balance': function (addressId) {
+    var balance = web3.eth.getBalance(addressId);
+    if (DEBUG) console.log("balance :" + balance);
+    return parseFloat(balance);
+  },
+  
+  'balanceInEther': function (addressId) {
+    var balanceInEther = web3.eth.getBalance(addressId);
+    if (DEBUG) console.log("balanceInEther :" + web3.fromWei(balanceInEther, 'ether'));
+    return parseFloat(web3.fromWei(balanceInEther, 'ether'));
+  },
+  
+  'getTransaction': function (transactionId) {
+    var number = web3.eth.blockNumber;
+    var transactionInfo =  web3.eth.getTransaction(transactionId);
+    transactionInfo.gasPrice = transactionInfo.gasPrice.c[0] + " WEI";
+    transactionInfo.ethValue = transactionInfo.value.c[0] / 10000;
+    transactionInfo.txprice = parseFloat((transactionInfo.gas * transactionInfo.gasPrice)/1000000000000000000) + " ETH";
+    // if (DEBUG) console.log("transactionInfo.txprice :" + transactionInfo.txprice);
+
+    if (transactionInfo.blockNumber != undefined) {
+      transactionInfo.conf = number - transactionInfo.blockNumber;
+      if (DEBUG) console.log("transactionInfo.conf :" + transactionInfo.conf);
+      if (transactionInfo.conf == 0) {
+        transactionInfo.conf = 'Unconfirmed';
+
+      }
+    }
+
+    if (transactionInfo.blockNumber != undefined) {
+      var info = web3.eth.getBlock(transactionInfo.blockNumber);
+      if (info != undefined) {
+        transactionInfo.time = info.timestamp;
+        // if (DEBUG) console.log("blockInfo.time :" + blockInfo.time);
+      }
+    }
+    return transactionInfo;
+  },
+
+}) // end of methods
+
+function hex2a(hexx) {
+  var hex = hexx.toString();//force conversion
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
+}
